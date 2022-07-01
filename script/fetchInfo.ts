df1247b720dacc5cd706f1991a843b0a60b390cd
mainnet-pat <mainnet_pat@protonmail.com>
2022-07-01 16:05:24 +0000

    Allow script to accept chain id, generate info for amber

--- script/fetchInfo.ts
+++ script/fetchInfo.ts
@@ -2,18 +2,30 @@ import { existsSync, fstat, readdirSync, readFileSync, writeFileSync } from "fs"
 
 import { ethers } from 'ethers'
 
-const ethersProvider = new ethers.providers.StaticJsonRpcProvider("https://global.uat.cash");
 const abi = [
   'function decimals() view returns (uint8)',
   'function symbol() view returns (string)',
   'function name() view returns (string)',
 ];
 
-export async function fetchInfo() {
-  const tokens = readdirSync('./blockchains/smartbch/assets');
+const rpcMap = {
+  10000: "https://global.uat.cash",
+  10001: "https://testnet.sonar.cash:9545"
+}
+
+const blockchainMap = {
+  10000: "smartbch",
+  10001: "smartbch-amber"
+}
+
+export async function fetchInfo(chainId) {
+  const blockchain = blockchainMap[chainId];
+  const ethersProvider = new ethers.providers.StaticJsonRpcProvider(rpcMap[chainId]);
+
+  const tokens = readdirSync(`./blockchains/${blockchain}/assets`);
   const newTokens = []
   for (const token of tokens) {
-    if (existsSync(`./blockchains/smartbch/assets/${token}/info.json`)) {
+    if (existsSync(`./blockchains/${blockchain}/assets/${token}/info.json`)) {
       continue;
     }
     console.log(token)
@@ -32,23 +44,23 @@ export async function fetchInfo() {
         address: token,
         symbol,
         decimals,
-        chainId: 10000,
-        logoURI: `https://raw.githubusercontent.com/mistswapdex/assets/master/blockchains/smartbch/assets/${token}/logo.png`,
+        chainId: chainId,
+        logoURI: `https://raw.githubusercontent.com/mistswapdex/assets/master/blockchains/${blockchain}/assets/${token}/logo.png`,
         website: "",
         description: "",
         explorer: "",
         status: "active",
         id: token,
         links: [],
-        type: "SMARTBCH"
+        type: blockchain.toUpperCase()
       }
       newTokens.push(entry);
-      writeFileSync(`./blockchains/smartbch/assets/${token}/info.json`, JSON.stringify(entry, null, 4));
+      writeFileSync(`./blockchains/${blockchain}/assets/${token}/info.json`, JSON.stringify(entry, null, 4));
     }
     catch {}
   }
 
-  const list = JSON.parse(readFileSync('./blockchains/smartbch/tokenlist.json', "utf-8"));
+  const list = JSON.parse(readFileSync(`./blockchains/${blockchain}/tokenlist.json`, "utf-8"));
   list.tokens = [...list.tokens, ...newTokens];
   const uniques = []
   list.tokens.forEach(val => {
@@ -58,7 +70,7 @@ export async function fetchInfo() {
   })
   list.tokens = uniques.sort((a, b) => a.id.localeCompare(b.id));
   list.version.minor++;
-  writeFileSync('./blockchains/smartbch/tokenlist.json', JSON.stringify(list, null, 4))
+  writeFileSync(`./blockchains/${blockchain}/tokenlist.json`, JSON.stringify(list, null, 4))
 }
 
-fetchInfo()
+fetchInfo(10001)
